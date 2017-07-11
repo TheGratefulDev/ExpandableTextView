f7cb97c802efc8524b21e22d93fe6bb19e4d926a
Ka Ling <Kling@Admins-MacBook-Pro.local>
2017-07-11 11:02:43 -0500

    initial commit

---
+++ Test/app/src/main/java/com/goeshow/test/ExpandableTextView.java
@@ -0,0 +1,98 @@
+package com.goeshow.test;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+
+/**
+ * Created by Kling on 7/11/17.
+ */
+
+public class ExpandableTextView extends AppCompatTextView {
+    private static final int DEFAULT_TRIM_LENGTH = 200;
+    private static final String DOTDOTDOT = "...";
+    private static final String ELLIPSIS = "\nRead more";
+
+    private CharSequence originalText;
+    private CharSequence trimmedText;
+    private TextView.BufferType bufferType;
+    private boolean trim = true;
+    private int trimLength;
+
+    public ExpandableTextView(Context context) {
+        this(context, null);
+    }
+
+    public ExpandableTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableTextView);
+        this.trimLength = typedArray.getInt(R.styleable.ExpandableTextView_trimLength, DEFAULT_TRIM_LENGTH);
+        typedArray.recycle();
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trim = !trim;
+                setText();
+                requestFocusFromTouch();
+            }
+        });
+    }
+
+    private void setText() {
+        super.setText(getDisplayableText(), bufferType);
+    }
+
+    private CharSequence getDisplayableText() {
+        return trim ? trimmedText : originalText;
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        originalText = text;
+        trimmedText = getTrimmedText(text);
+        bufferType = type;
+        setText();
+    }
+
+    private CharSequence getTrimmedText(CharSequence text) {
+        if (originalText != null && originalText.length() > trimLength) {
+
+            SpannableStringBuilder builder = new SpannableStringBuilder(originalText, 0, trimLength + 1);
+
+            builder.append(DOTDOTDOT);
+
+            SpannableString colorEllipsis = new SpannableString(ELLIPSIS);
+            colorEllipsis.setSpan(new ForegroundColorSpan(Color.BLUE), 0, colorEllipsis.length(), 0);
+            builder.append(colorEllipsis);
+
+
+            return builder;
+        } else {
+            return originalText;
+        }
+    }
+
+    public CharSequence getOriginalText() {
+        return originalText;
+    }
+
+    public void setTrimLength(int trimLength) {
+        this.trimLength = trimLength;
+        trimmedText = getTrimmedText(originalText);
+        setText();
+    }
+
+    public int getTrimLength() {
+        return trimLength;
+    }
+}
